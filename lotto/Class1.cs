1783a8d615cab6278be5aec579cbb49db182a225
Juro12 <54584470+Juro12@users.noreply.github.com>
2019-08-28 12:13:26 +0200

    lotto c# ver. 1.0

---
+++ lotto/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lotto
+{
+    class losowanie
+    {
+        public int[] liczba;
+        public int[] strzal;
+        public int wynik = 0;
+        public double wygrana = 0;
+        public int skresl = 0;
+        public int kumulacja = 0;
+
+        public void licz_wynik() {
+            switch (wynik)
+            {
+                case 6: Console.WriteLine("Trafiłeś wrzystkie 6 cyfr"); break;
+                case 5: Console.WriteLine("Trafiłeś 5 cyfr"); break;
+                case 4: Console.WriteLine("Trafiłeś 4 cyfry"); break;
+                case 3: Console.WriteLine("Trafiłeś 3 cyfry"); break;
+                case 2: Console.WriteLine("Trafiłeś 2 cyfry"); break;
+                case 1: Console.WriteLine("Trafiłeś 1 cyfrę"); break;
+                case 0: Console.WriteLine("Nie trafiłeś żadnej cyfry"); break;
+            }
+        }
+
+        public void licz_wygrana()
+        {
+            switch (wynik)
+            {
+                case 6: wygrana = 1 * kumulacja; break;
+                case 5: wygrana = 0.00084 * kumulacja; break;
+                case 4: wygrana = 0.00035 * kumulacja; break;
+                case 3: wygrana = 0.00002 * kumulacja; break;
+                case 2: wygrana = 0.000014 * kumulacja; break;
+                case 1: wygrana = 0.000012 * kumulacja; break;
+                case 0: wygrana = 0 * kumulacja; break;
+            }
+
+            wygrana = Math.Round(wygrana * 100) / 100;
+
+            if (wynik == 6) { Console.WriteLine("Wygrałeś maksymalną wygraną! Gratulacje oto twoje " + wygrana + " złotych!"); }
+            else if (wynik == 0) { Console.WriteLine("Niestety nie wygrałeś nic."); }
+            else { Console.WriteLine("Wygrałeś " + wygrana + " złotych!"); }
+        }
+
+    }
+}
---
+++ lotto/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading;
+
+namespace lotto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            losowanie los = new losowanie();
+
+            los.liczba = new int[6];
+            los.strzal = new int[6];
+
+            Random generator = new Random();
+
+            Console.WriteLine("Witaj w kolejnym losowaniu lotto!");
+            Thread.Sleep(2000);
+            Console.WriteLine("Dzis do wygrania jest: ");
+            los.kumulacja = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= 5; i++) {
+                los.liczba[i] = generator.Next(1,50);
+
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (los.liczba[j] == los.liczba[i])
+                    {
+                        i--;
+                    }
+                }
+            }
+            Console.WriteLine("Podaj swoje typy! Liczby w przedziale od 1 do 49.");
+
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("Podaj "+(i+1)+" liczbe:");
+                los.strzal[i] = int.Parse(Console.ReadLine());
+
+                if (los.strzal[i] <= 0 || los.strzal[i] >= 50)
+                {
+                    Console.WriteLine("Nie morzesz skreślić podanej liczby! Znajduje się ona za pzedziałem 1-49!");
+                    Console.WriteLine("Podaj liczbę "+(i+1)+" ponownie.");
+                    i--;
+                }
+
+                for (int j = i-1; j >= 0; j--)
+                {
+                    if (los.strzal[j] == los.strzal[i])
+                    {
+                        Console.WriteLine("Juz skresliles ta liczbe skresl inna!");
+                        i--;
+                    }
+                }
+            }
+
+            for (int i = 0; i <= 5; i++) {
+                for (int j = 0; j <= 5; j++) {
+                    if (los.liczba[i] == los.strzal[j]) {
+                        los.wynik++;
+                    }
+                }
+            }
+
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Zachwile nastąpi zwolnienie blokady!");
+            Thread.Sleep(2000);
+            Console.WriteLine("Wylosowane liczby to: " + los.liczba[0] + ", " + los.liczba[1] + ", " + los.liczba[2] + ", " + los.liczba[3] + ", " + los.liczba[4] + ", " + los.liczba[5]);
+            Thread.Sleep(1000);
+            Console.WriteLine("Twoje typy to: " + los.strzal[0] + ", " + los.strzal[1] + ", " + los.strzal[2] + ", " + los.strzal[3] + ", " + los.strzal[4] + ", " + los.strzal[5]);
+            Thread.Sleep(1000);
+            los.licz_wynik();
+            Thread.Sleep(1000);
+            los.licz_wygrana();
+        }
+    }
+}
